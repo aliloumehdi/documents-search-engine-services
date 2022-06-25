36ea08ebe4e41d66391b6e660d2f8b3ec31eec9e
moustahesane <moustahsane.achraf@ofppt-edu.ma>
2022-06-25 15:31:35 +0100

    fileupload & services arch

--- app.js
+++ app.js
@@ -3,12 +3,18 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+const fileUpload = require('express-fileupload');
+const bodyParser = require('body-parser');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var docsRouter = require('./routes/docs');
 
 var app = express();
 
+
+app.use(fileUpload({
+  createParentPath: true
+}));
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -18,9 +24,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/docs', docsRouter);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
