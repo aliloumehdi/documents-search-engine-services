24fdef008e4f2dc72e63b4fc6e6f1cf939db962f
moustahesane <moustahsane.achraf@ofppt-edu.ma>
2022-06-26 23:45:07 +0800

    rest docsController

---
+++ controllers/docsController.js
@@ -0,0 +1,111 @@
+
+var extractors = require('../services/extractor');
+var elstic = require('../eslaticnode/server');
+var thumb = require('../thumbnail/generator');
+var toPdf = require("office-to-pdf")
+var fs = require('fs')
+const { v4: uuidv4 } = require('uuid');
+exports.index = function (req, res) {
+    res.send('NOT IMPLEMENTED: Site Home Page');
+};
+
+exports.upload = async (req, res) => {
+
+    try {
+
+
+        if (!req.files) {
+            res.send({
+                status: false,
+                message: 'No file uploaded'
+            });
+        } else {
+
+            let uuid = uuidv4();
+            //Use the name of the input field (i.e. "avatar") to retrieve the uploaded file
+            let rawdoc = req.files.rawdoc;
+            // console.log(req.files.rawdoc.path);
+
+            //Use the mv() method to place the file in upload directory (i.e. "uploads")
+            const tempPath = './public/files/' + uuid+"_"+rawdoc.name
+            await rawdoc.mv(tempPath);
+
+
+
+            
+            // var wordBuffer = fs.readFileSync(tempPath)
+
+            // toPdf(wordBuffer).then(
+            //   (pdfBuffer) => {
+            //     console.log("pdfBuffer");
+            //     fs.writeFileSync("test.pdf", pdfBuffer)
+            //   }, (err) => {
+            //     console.log(err)
+            //   }
+            // )
+
+
+
+
+
+
+            const text = await extractTexte(rawdoc, tempPath,uuid);
+
+
+            elstic.post()
+            
+            
+            //send response
+            res.send({
+                status: true,
+                message: 'File is uploaded',
+                data: {
+                    name: rawdoc.name,
+                    mimetype: rawdoc.mimetype,
+                    size: rawdoc.size,
+                    text : text
+                }
+            });
+
+            
+
+            
+        }
+    } catch (err) {
+        res.status(500).send(err);
+    }
+}
+
+const extractTexte = async (doc, tempPath,uuid) => {
+
+
+    return new Promise((resolve, reject) => {
+
+        if (doc.mimetype == "application/pdf") {
+
+            extractors.pdfextractor(doc, tempPath,uuid).then(res => {
+
+
+                console.log("PDF")
+                resolve(res)
+            })
+
+        } else if (doc.mimetype.split('/')[0] == "image") {
+            extractors.imageExtractor(doc, tempPath,uuid).then(res => {
+                console.log("IMG")
+
+                resolve(res)
+                console.log("IMG ----------")
+                thumb.thumbimg(tempPath,uuid)
+            })
+        } else {
+
+            extractors.officeExtrctor(tempPath, tempPath,uuid).then(res => {
+                console.log("OFFICE")
+
+                resolve(res)
+            })
+        }
+
+    });
+}
