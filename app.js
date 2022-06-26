7a5eaf74daa94db516b9585ae4fa6d3c4fef718f
moustahesane <moustahsane.achraf@ofppt-edu.ma>
2022-06-26 20:18:26 +0800

    file text extraction done formats [Images/PDF/Office]

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 const fileUpload = require('express-fileupload');
 const bodyParser = require('body-parser');
 var indexRouter = require('./routes/index');
@@ -21,6 +22,7 @@ app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
